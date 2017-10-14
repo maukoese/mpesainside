b7e18f53bf4e1f56bcf48ae8238d7817970ebd0f
Mauko Maunde <maukoese@gmail.com>
2017-10-14 14:28:42 +0300

    Needed

--- MPESA.php
+++ MPESA.php
@@ -2,9 +2,28 @@
 
 class MPESA{
 
-	function __construct( $key, $secret ){
+	private business;
+	private shortcode;
+	private key;
+	private secret;
+	private password;
+	private publicKey;
+	private timeout_url;
+	private result_url;
+	private confirmation_url;
+	private validation_url;
+
+	public function __construct( $name, $shortcode, $key, $secret, $pass, $public_key, $timeout_url, $result_url, $confirmation_url, $validation_url ){
+		$this -> business = $name;
+		$this -> shortcode = $shortcode;
 		$this -> key = $key;
 		$this -> secret = $secret;
+		$this -> password = $pass;
+		$this -> publicKey = $public_key;
+		$this -> timeout_url = $timeout_url;
+		$this -> result_url = $result_url;
+		$this -> confirmation_url = $confirmation_url;
+		$this -> validation_url = $validation_url;
 	}
 
 	private function authenticate(){
@@ -22,17 +41,285 @@ class MPESA{
 		return json_decode($curl_response);
 	}
 
-	function setcred(){
-		$publicKey = "PATH_TO_CERTICATE";
-		//$plaintext = "Safaricom132!";
-		$plaintext = "YOUR_PASSWORD";
+	private function securityCredential(){
 
-		openssl_public_encrypt($plaintext, $encrypted, $publicKey, OPENSSL_PKCS1_PADDING);
+		openssl_public_encrypt( $this -> pass, $encrypted, $this -> publicKey, OPENSSL_PKCS1_PADDING );
 
-		echo base64_encode($encrypted);
+		return base64_encode( $encrypted );
+	}
+
+	private function b2cRequest( $initiator, $CommandID, $Amount, $PartyB, $Remarks = "", $Occasion = "" ){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'InitiatorName' => $InitiatorName,
+		  'SecurityCredential' => $this -> securityCredential()
+		  'CommandID' => $CommandID,
+		  'Amount' => $Amount,
+		  'PartyA' => $this -> business,
+		  'PartyB' => $PartyB,
+		  'Remarks' => $Remarks,
+		  'QueueTimeOutURL' => $this -> timeout_url,
+		  'ResultURL' => $this -> result_url,
+		  'Occasion' => $Occasion
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		return $curl_response;
+	}
+
+	private function b2bRequest( $initiator, $CommandID, $Amount, $PartyB, $SenderIdentifierType, $RecieverIdentifierType, $AccountReference, $Remarks = "", $Occasion = "" ){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'Initiator' => $InitiatorName,
+		  'SecurityCredential' => $this -> securityCredential(),
+		  'CommandID' => $CommandID,
+		  'SenderIdentifierType' => $SenderIdentifierType,
+		  'RecieverIdentifierType' => $RecieverIdentifierType,
+		  'Amount' => $Amount,
+		  'PartyA' => $this -> business,
+		  'PartyB' => $PartyB,
+		  'AccountReference' => $AccountReference,
+		  'Remarks' => $Remarks,
+		  'QueueTimeOutURL' => $this -> timeout_url,
+		  'ResultURL' => $this -> result_url,
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		return $curl_response;
+	}
+
+	private function c2b( $ResponseType = "Application/json" ){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'ShortCode' => $this -> shortcode,
+		  'ResponseType' => $ResponseType,
+		  'ConfirmationURL' => $this -> confirmation_url,
+		  'ValidationURL' => $this -> validation_url
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		echo $curl_response;
+	}
+
+	public function simulate(){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+		$curl_post_data = array(
+		      //Fill in the request parameters with valid values
+		     'ShortCode' => ' ',
+		     'CommandID' => 'CustomerPayBillOnline',
+		     'Amount' => ' ',
+		     'Msisdn' => ' ',
+		     'BillRefNumber' => '00000'
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		echo $curl_response;
+	}
+
+	function accountBalance(){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'CommandID' => ' ',
+		  'Initiator' => ' ',
+		  'SecurityCredential' => ' ',
+		  'CommandID' => 'AccountBalance',
+		  'PartyA' => ' ',
+		  'IdentifierType' => '4',
+		  'Remarks' => ' ',
+		  'QueueTimeOutURL' => 'https://ip_address:port/timeout_url',
+		  'ResultURL' => 'https://ip_address:port/result_url'
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		echo $curl_response;
+	}
+
+	public function reverseTransaction( $value='' ){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'CommandID' => ' ',
+		  'Initiator' => ' ',
+		  'SecurityCredential' => ' ',
+		  'CommandID' => 'TransactionReversal',
+		  'TransactionID' => ' ',
+		  'Amount' => ' ',
+		  'ReceiverParty' => ' ',
+		  'RecieverIdentifierType' => '4',
+		  'ResultURL' => 'https://ip_address:port/result_url',
+		  'QueueTimeOutURL' => 'https://ip_address:port/timeout_url',
+		  'Remarks' => ' ',
+		  'Occasion' => ' '
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		echo $curl_response;
+	}
+
+	public function queryRequest( $value='' ){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'BusinessShortCode' => ' ',
+		  'Password' => ' ',
+		  'Timestamp' => ' ',
+		  'CheckoutRequestID' => ' '
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		echo $curl_response;
+	}
+
+
+	function transactionStatus(){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'Initiator' => ' ',
+		  'SecurityCredential' => ' ',
+		  'CommandID' => 'TransactionStatusQuery',
+		  'TransactionID' => ' ',
+		  'PartyA' => ' ',
+		  'IdentifierType' => '1',
+		  'ResultURL' => 'https://ip_address:port/result_url',
+		  'QueueTimeOutURL' => 'https://ip_address:port/timeout_url',
+		  'Remarks' => ' ',
+		  'Occasion' => ' '
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		echo $curl_response;
+	}
+
+	public function errors($value=''){
+		$postData = = file_get_contents('php://input');
+	    //perform your processing here, e.g. log to file....
+	    $file = fopen("log.txt", "w"); //url fopen should be allowed for this to occur
+	    if(fwrite($file, $postData) === FALSE)
+	    {
+	        fwrite("Error: no data written");
+	    }
+
+	    fwrite("\r\n");
+	    fclose($file);
+
+	    echo '{"ResultCode": 0, "ResultDesc": "The service was accepted successfully", "ThirdPartyTransID": "1234567890"}';
+
 	}
 
 }
-
-$mpesa = new MPESA('l6jE7kgV4lCtNH4aveMueR9QdGkbutfR', '5slRuAafb4Gk7Ogo');
-?>
--- README.md
+++ README.md
@@ -1,2 +1,159 @@
-# mpesapay
-Simplified Lipa Na MPESA Online.
+# MPESAOPay
+Simplified Lipa Na MPESA Online integration.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+A running PHP server.
+A database system - MySQL/MariaDB/SQLite/PostgreSQL/Mongo
+
+```
+Give examples
+```
+
+### Installing
+
+Grab a copy of Jabali from here
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* [Material Design](http://material.io/) - The web framework used
+* [JQuery](https://code.google.com/jquery/) - Dependency Management
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [CalVer]( http://calver.org ) for versioning. For the versions available, see the [tags on this repository](https://github.com/maukoese/jabali/tags). 
+
+## Authors
+
+* **Mauko Maunde** - *Initial work* - [Jabali CMS](https://github.com/maukoese/jabali)
+
+See also the list of [contributors](https://github.com/maukoese/jabali/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
+
+## Command ID Description
+* [TransactionReversal]	Reversal for an erroneous C2B transaction.
+* [SalaryPayment] Used to send money from an employer to employees e.g. salaries
+* [BusinessPayment] Used to send money from business to customer e.g. refunds
+* [PromotionPayment] Used to send money when promotions take place e.g. raffle winners
+* [AccountBalance] Used to check the balance in a paybill/buy goods account (includes utility, MMF, Merchant, Charges paid account).
+* [CustomerPayBillOnline] Used to simulate a transaction taking place in the case of C2B Simulate Transaction or to initiate a transaction on behalf of the customer (STK Push).
+* [TransactionStatusQuery] Used to query the details of a transaction.
+* [CheckIdentity]	Similar to STK push, uses M-Pesa PIN as a service.
+* [BusinessPayBill]	Sending funds from one paybill to another paybill
+* [BusinessBuyGoods]	sending funds from buy goods to another buy goods.
+* [DisburseFundsToBusiness]	Transfer of funds from utility to MMF account.
+* [BusinessToBusinessTransfer]	Transferring funds from one paybills MMF to another paybills MMF account.
+* [BusinessTransferFromMMFToUtility]	Transferring funds from paybills MMF to another paybills utility account.
+
+B2C Query Parameters
+
+Parameter	Description
+InitiatorName	This is the credential/username used to authenticate the transaction request.
+SecurityCredential	Base64 encoded string of the B2C short code and password, which is encrypted using M-Pesa public key and validates the transaction on M-Pesa Core system.
+CommandID	Unique command for each transaction type e.g. SalaryPayment, BusinessPayment, PromotionPayment
+Amount	The amount being transacted
+PartyA	Organization’s shortcode initiating the transaction.
+PartyB	Phone number receiving the transaction
+Remarks	Comments that are sent along with the transaction.
+QueueTimeOutURL	The timeout end-point that receives a timeout response.
+ResultURL	The end-point that receives the response of the transaction
+Occasion	Optional
+
+B2B - Request Parameters
+
+Parameter	Description
+Initiator	This is the credential/username used to authenticate the transaction request.
+SecurityCredential	Base64 encoded string of the B2B short code and password, which is encrypted using M-Pesa public key and validates the transaction on M-Pesa Core system.
+CommandID	Unique command for each transaction type, possible values are: BusinessPayBill, MerchantToMerchantTransfer, MerchantTransferFromMerchantToWorking, MerchantServicesMMFAccountTransfer, AgencyFloatAdvance
+Amount	The amount being transacted.
+PartyA	Organization’s short code initiating the transaction.
+SenderIdentifier	Type of organization sending the transaction.
+PartyB	Organization’s short code receiving the funds being transacted.
+RecieverIdentifierType	Type of organization receiving the funds being transacted.
+Remarks	Comments that are sent along with the transaction.
+QueueTimeOutURL	The path that stores information of time out transactions.it should be properly validated to make sure that it contains the port, URI and domain name or publicly available IP.
+ResultURL	The path that receives results from M-Pesa it should be properly validated to make sure that it contains the port, URI and domain name or publicly available IP.
+AccountReference	Account Reference mandatory for “BusinessPaybill” CommandID.
+
+B2B Response Parameters
+
+Parameter	Description
+ConversationID	A unique numeric code generated by the M-Pesa system of the response to a request.
+OriginatorConversationID	A unique numeric code generated by the M-Pesa system of the request.
+ResponseDescription	A response message from the M-Pesa system accompanying the response to a request.
+
+C2B Register URL - Request Parameters
+
+Parameter	Description
+ValidationURL	Validation URL for the client.
+ConfirmationURL	Confirmation URL for the client.
+ResponseType	Default response type for timeout.
+ShortCode	The short code of the organization.
+Register URL - Response Parameters
+
+Parameter	Description
+ConversationID	A unique numeric code generated by the M-Pesa system of the response to a request.
+OriginatorConversationID	A unique numeric code generated by the M-Pesa system of the request.
+ResponseDescription	A response message from the M-Pesa system accompanying the response to a request.
+
+## Identifier Types
+
+Identifier types - both sender and receiver - identify an M-Pesa transaction’s sending and receiving party as either a shortcode, a till number or a MSISDN (phone number). There are three identifier types that can be used with M-Pesa APIs.
+
+Identifier	Identity
+1	MSISDN
+2	Till Number
+4	Shortcode
