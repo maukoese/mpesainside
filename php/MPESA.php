2c7dd60ea3d6936140ec58298d65ca8c2cb53315
Mauko Maunde <maukoese@gmail.com>
2017-10-14 16:34:56 +0300

    Added Online Payment Method

--- php/MPESA.php
+++ php/MPESA.php
@@ -290,6 +290,41 @@ class MPESA{
 
 		echo $curl_response;
 	}
+	
+	public function pay(){
+		$url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+
+
+		$curl_post_data = array(
+		  //Fill in the request parameters with valid values
+		  'BusinessShortCode' => ' ',
+		  'Password' => ' ',
+		  'Timestamp' => ' ',
+		  'TransactionType' => 'CustomerPayBillOnline',
+		  'Amount"' => ' ',
+		  'PartyA' => ' ',
+		  'PartyB' => ' ',
+		  'PhoneNumber' => ' ',
+		  'CallBackURL' => 'https://ip_address:port/callback',
+		  'AccountReference' => ' ',
+		  'TransactionDesc' => ' '
+		);
+
+		$data_string = json_encode($curl_post_data);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$curl_response = curl_exec($curl);
+		print_r($curl_response);
+
+		echo $curl_response;
+	}
 
 
 	public function transactionStatus(){
